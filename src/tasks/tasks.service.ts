f943ee6cd9eb0c4370688f3c7cc83fef5a23dc47
codebyalok <engg.alokg@gmail.com>
2020-06-08 00:43:23 +0100

    delete task and update task status functions added

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -12,6 +12,27 @@ export class TasksService {
     return this.tasks;
   }
 
+  getTaskById(id: string): Task {
+
+   return this.tasks.find(task => task.id===id);
+
+  }
+
+  deleteTask(id: string): void{
+     this.tasks=this.tasks.filter(task => task.id!=id);
+
+
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus): Task{
+    const task = this.getTaskById(id);
+    task.status=status;
+    return task;
+
+  }
+
+
+
   createTask (createTaskDto : CreateTaskDto) : Task{
 const {title, description} = createTaskDto;
 
