dbff5b378eb0e5a97c7bf1c14189b97c6f5757fa
codebyalok <engg.alokg@gmail.com>
2020-06-12 23:15:29 +0100

    adding filer for get tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,15 +1,26 @@
-import { Controller, Get, Post, Body, Param, Delete, Patch } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch, Query } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskFilterDto } from './dto/task-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor (private tasksService:TasksService) {}
 
   @Get()
-  getAllTasks(): Task[]{
-    return this.tasksService.getAllTasks();
+  getTasks(@Query() taskfilterDto: TaskFilterDto): Task[]{
+
+    if (Object.keys(taskfilterDto).length)
+    {
+      return this.tasksService.getTasksWithFilter(taskfilterDto);
+      
+    }
+    else
+    {
+      return this.tasksService.getAllTasks();
+    }
+
   }
 
   @Get('/:id')
